6fe85cc5728010700568b271a002a6ecd58f8040
RPUMPO\rpump <rathapong.now@gmail.com>
2026-04-04 00:10:47 +0700

    add icon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,7 +11,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@400;500;600;700;800&family=Sora:wght@500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
     <link rel="icon" type="image/svg+xml" href="{{ asset('assets/icons/playflowpos-mark.svg') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
     <link href="{{ asset('assets/css/landing.css') }}" rel="stylesheet">
 </head>
 <body class="pf-page">
